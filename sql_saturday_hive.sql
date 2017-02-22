145129ef78e89754f6256a85b998c25b28b5eeae
Dan Zaratsian <dtz001@gmail.com>
2017-02-22 09:33:55 -0500

    Update sql_saturday_hive.sql

--- sql_saturday_hive.sql
+++ sql_saturday_hive.sql
@@ -7,8 +7,8 @@ SHOW TABLES;
 --#
 --###############################################################################################################
 
-CREATE EXTERNAL TABLE IF NOT EXISTS mm_season
-    (season string, day string, wteam string, wscore int, lteam string, lscore int, wloc string, ot string)
+CREATE EXTERNAL TABLE IF NOT EXISTS mm_season_temp
+    (id string, season string, day string, wteam string, wscore int, lteam string, lscore int, wloc string, ot string)
     ROW FORMAT DELIMITED
     FIELDS TERMINATED BY ","
     LINES TERMINATED BY "\n"
@@ -16,24 +16,24 @@ CREATE EXTERNAL TABLE IF NOT EXISTS mm_season
     LOCATION "/tmp/marchmadness/SeasonResults"
     TBLPROPERTIES ("skip.header.line.count=1");
 
-CREATE TABLE IF NOT EXISTS mm_season_orc
-    (season string, day string, wteam string, wscore int, lteam string, lscore int, wloc string, ot string)
+CREATE TABLE IF NOT EXISTS mm_season
+    (id string, season string, day string, wteam string, wscore int, lteam string, lscore int, wloc string, ot string)
     ROW FORMAT DELIMITED
     FIELDS TERMINATED BY ","
     LINES TERMINATED BY "\n"
     STORED AS ORC;
 
-INSERT OVERWRITE TABLE mm_season_orc SELECT * FROM mm_season;
+INSERT OVERWRITE TABLE mm_season SELECT * FROM mm_season_temp;
 
-SELECT * FROM mm_season_orc LIMIT 10;
+SELECT * FROM mm_season LIMIT 10;
 
-SELECT COUNT(*) FROM mm_season_orc;
+SELECT COUNT(*) FROM mm_season;
 
-DESCRIBE mm_season_orc;
+DESCRIBE mm_season;
 
 --###############################################################################################################
 
-CREATE EXTERNAL TABLE IF NOT EXISTS mm_teams
+CREATE EXTERNAL TABLE IF NOT EXISTS mm_teams_temp
     (team_id string, team_name string)
     ROW FORMAT DELIMITED
     FIELDS TERMINATED BY ","
@@ -42,20 +42,20 @@ CREATE EXTERNAL TABLE IF NOT EXISTS mm_teams
     LOCATION "/tmp/marchmadness/Teams"
     TBLPROPERTIES ("skip.header.line.count=1");
 
-CREATE TABLE IF NOT EXISTS mm_teams_orc
+CREATE TABLE IF NOT EXISTS mm_teams
     (team_id string, team_name string)
     ROW FORMAT DELIMITED
     FIELDS TERMINATED BY ","
     LINES TERMINATED BY "\n"
     STORED AS ORC;
 
-INSERT OVERWRITE TABLE mm_teams_orc SELECT * FROM mm_teams;
+INSERT OVERWRITE TABLE mm_teams SELECT * FROM mm_teams_temp;
 
-SELECT * FROM mm_teams_orc LIMIT 10;
+SELECT * FROM mm_teams LIMIT 10;
 
-SELECT COUNT(*) FROM mm_teams_orc;
+SELECT COUNT(*) FROM mm_teams;
 
-DESCRIBE mm_teams_orc;
+DESCRIBE mm_teams;
 
 --###############################################################################################################
 --#
@@ -64,19 +64,19 @@ DESCRIBE mm_teams_orc;
 --###############################################################################################################
 
 CREATE TABLE IF NOT EXISTS mm_join1 AS
-    SELECT mm_season_orc.*, 
+    SELECT mm_season.*, 
         teams2a.team_name AS WTEAM_NAME, 
         teams2b.team_name AS LTEAM_NAME
-    FROM mm_season_orc
-    LEFT JOIN mm_teams_orc teams2a ON (mm_season_orc.wteam = teams2a.team_id)
-    LEFT JOIN mm_teams_orc teams2b ON (mm_season_orc.lteam = teams2b.team_id);
+    FROM mm_season
+    LEFT JOIN mm_teams teams2a ON (mm_season.wteam = teams2a.team_id)
+    LEFT JOIN mm_teams teams2b ON (mm_season.lteam = teams2b.team_id);
 
 SELECT * FROM mm_join1 LIMIT 10;
     
 DESC mm_join1;
 
-SELECT COUNT(*) FROM mm_season_orc;
-SELECT COUNT(*) FROM mm_teams_orc;
+SELECT COUNT(*) FROM mm_season;
+SELECT COUNT(*) FROM mm_teams;
 SELECT COUNT(*) FROM mm_join1;
 
 --SHOW CREATE TABLE mm_join1;
@@ -88,35 +88,42 @@ SELECT COUNT(*) FROM mm_join1;
 --###############################################################################################################
 
 CREATE VIEW IF NOT EXISTS mm_join1_view AS
-    SELECT mm_season_orc.*, 
+    SELECT mm_season.*, 
         teams2a.team_name AS WTEAM_NAME, 
         teams2b.team_name AS LTEAM_NAME
-    FROM mm_season_orc
-    LEFT JOIN mm_teams_orc teams2a ON (mm_season_orc.wteam = teams2a.team_id)
-    LEFT JOIN mm_teams_orc teams2b ON (mm_season_orc.lteam = teams2b.team_id);
+    FROM mm_season
+    LEFT JOIN mm_teams teams2a ON (mm_season.wteam = teams2a.team_id)
+    LEFT JOIN mm_teams teams2b ON (mm_season.lteam = teams2b.team_id);
+
 
 --###############################################################################################################
 --#
---#   Hive - Aggregation
+--#   Hive - Calculations
 --#
 --###############################################################################################################
 
+
+-- Calculate the Top 15 Teams with the most Wins
 SELECT WTEAM_NAME, COUNT(*) AS WINS 
     FROM mm_join1 
     GROUP BY WTEAM_NAME 
     ORDER BY WINS DESC 
     LIMIT 15;
 
+
+-- Calculate the Top 15 Teams with the most Losses
 SELECT LTEAM_NAME, COUNT(*) AS LOSSES 
     FROM mm_join1 
     GROUP BY LTEAM_NAME 
     ORDER BY LOSSES DESC 
     LIMIT 15;
 
+
+-- Calculate the Top 15 Matchups with the biggest score difference
 SELECT SEASON, WSCORE, LSCORE, WLOC, (WSCORE-LSCORE) AS SCORE_DIFF, CONCAT(WTEAM_NAME, " OVER ", LTEAM_NAME) as DESC
     FROM mm_join1
     ORDER BY SCORE_DIFF DESC
-    LIMIT 10;
+    LIMIT 15;
 
 
 --#ZEND
