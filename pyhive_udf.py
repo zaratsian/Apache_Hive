3678414b4d67ec89c6ecf231dc6d6cfa63ffe4e0
Dan Zaratsian <dtz001@gmail.com>
2017-07-10 08:53:09 -0400

    Create pyhive_udf.py

---
+++ pyhive_udf.py
@@ -0,0 +1,54 @@
+
+################################################################################################
+#
+#   Python UDF for Apache Hive
+#
+#   INPUT:  tab delimited string received via stdin
+#   OUTPUT: tab delimited string sent via print/stdout
+#
+################################################################################################
+
+import sys
+
+for line in sys.stdin:
+    
+    id, text = line.split('\t')
+    
+    positive = set(["love", "good", "great", "happy", "cool", "best", "awesome", "nice"])
+    negative = set(["hate", "bad", "stupid"])
+    
+    words = text.split()
+    word_count = len(words)
+    
+    positive_matches = [1 for word in words if word in positive]
+    negative_matches = [-1 for word in words if word in negative]
+    
+    st = sum(positive_matches) + sum(negative_matches)
+    
+    if st > 0:
+        print '\t'.join([id, text, 'positive', str(word_count)])
+    elif st < 0:
+        print '\t'.join([id, text, 'negative', str(word_count)])
+    else:
+        print '\t'.join([id, text, 'neutral', str(word_count)])
+
+
+################################################################################################
+#
+#   Corresponding Hive Script
+#
+################################################################################################
+
+'''
+ADD FILE /home/hive/my_py_udf.py;
+SELECT
+TRANSFORM (id, text)
+USING 'python pyfunc3.py'
+AS  id,  
+    text, 
+    sentiment,
+    word_count
+FROM tweets;
+'''
+
+#ZEND
